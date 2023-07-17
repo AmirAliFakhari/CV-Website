7e89ab6023607edfa5fbac1ab40a4d59ec9740fe
AmirAli <aafz1382@gmail.com>
2023-07-17 11:10:28 +0330

    upd

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 
-
-  const aboutMe = document.getElementById('aboutMe')
-  aboutMe.scrollTo({
-    top:200,
-    left:100.,
-    behavior:"smooth"
-  })
+  // Get the navbar element
+var navbar = document.getElementById("navbarSupportedContent");
+console.log(navbar)
+// Get the toggler button
+var toggler = document.querySelector(".navbar-toggler");
+console.log(toggler)
+// Add an event listener to the document
+document.addEventListener("click", function(event) {
+  // Check if the target element is not inside the navbar or the toggler button
+  if (!navbar.contains(event.target) && !toggler.contains(event.target)) {
+    // Collapse the navbar if it is expanded
+    if (navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
+  }
+});
