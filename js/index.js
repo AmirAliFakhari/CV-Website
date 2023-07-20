bd0199b5a162b89631190d6f33e9253ad62cd5e8
amir z <aafz1382@gmail.com>
2023-07-21 00:59:28 +0330

    upd

--- js/index.js
+++ js/index.js
@@ -10,3 +10,7 @@ document.addEventListener("click", function (event) {
     }
   }
 });
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
