41cae5a486974ab213159064a0028867bcbc5a44
amir z <aafz1382@gmail.com>
2023-07-21 21:11:36 +0330

    upd

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var navbar = document.getElementById("navbarSupportedContent");
-console.log(navbar)
 var toggler = document.querySelector(".navbar-toggler");
-console.log(toggler)
 document.addEventListener("click", function (event) {
   if (!navbar.contains(event.target) && !toggler.contains(event.target)) {
     // Collapse the navbar if it is expanded
